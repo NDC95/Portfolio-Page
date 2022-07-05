13fe4ce86528a25c2bd01651777d29fd7607bd7c
Nick <nbnetto@hotmail.com>
2022-07-05 14:13:39 +0100

    added email functionality

---
+++ mail.php
@@ -0,0 +1,18 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+
+$to = "nbnetto@hotmail.com";
+
+$subject = "Mail From Portfolio website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@ndc95.github.io/Portfolio-Page/" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
