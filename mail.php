6f6f00989b7896abea13d0521926509123443d09
Nick <nbnetto@hotmail.com>
2022-07-05 14:21:24 +0100

    fix

--- mail.php
+++ mail.php
@@ -8,7 +8,7 @@ $to = "nbnetto@hotmail.com";
 
 $subject = "Mail From Portfolio website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@ndc95.github.io/Portfolio-Page/" . "\r\n" .
+$headers = "From: noreply@https://ndc95.github.io/Portfolio-Page/" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
